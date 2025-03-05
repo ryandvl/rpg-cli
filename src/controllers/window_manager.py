97b6b45c1b520e9dc3133a7daccd06e402464c3a
ryandvl <ryandvl@proton.me>
2025-03-05 10:50:28 -0300

    docs: add WindowManager documentation

--- src/controllers/window_manager.py
+++ src/controllers/window_manager.py
@@ -16,12 +16,21 @@ class WindowManager:
     last_window: str = 'default'
 
     def setup(self, game_manager: 'GameManager') -> None:
+        '''
+        Initialize all necessary modules to run WindowManager
+        '''
         self.game_manager = game_manager        
 
     def get_window(self, name: str) -> WindowInterface | None:
+        '''
+        Get a stored window
+        '''
         return self.windows.get(name)
 
     def register_window(self, name: str, window: curses.window) -> WindowInterface:
+        '''
+        Register and store a window
+        '''
         window_interface = WindowInterface()
         window_interface.name = name
         window_interface.window = window
@@ -34,9 +43,15 @@ class WindowManager:
         return window_interface
 
     def unregister_window(self, name: str) -> None:
+        '''
+        Unregister a window
+        '''
         del self.windows[name]
 
     def change_window(self, name: str) -> None:
+        '''
+        Change current window, this will clear the screen
+        '''
         window_interface = self.get_window(name)
 
         if not window_interface: return
